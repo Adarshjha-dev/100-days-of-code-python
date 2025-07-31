30274cdb10e49dee9be17fe2418634ba960221ce
Adarsh Jha <jhaadarsh924@gmail.com>
2025-08-01 00:15:46 +0530

    Add data manager for retrieving and updating sheet data

---
+++ Day39/data_manager.py
@@ -0,0 +1,36 @@
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+SHEETY_ENDPOINT = os.environ["SHEETY_ENDPOINT"]
+
+class DataManager:
+
+    def __init__(self):
+        self._user = os.environ["SHEETY_USERNAME"]
+        self._password = os.environ["SHEETY_PASSWORD"]
+        self._auth = HTTPBasicAuth(self._user, self._password)
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_ENDPOINT, auth=self._auth)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_ENDPOINT}/{city['id']}",
+                json=new_data,
+            )
+            print(response.text)
