fd03bde0e516c2bcbb28a522a811cc17512be7ab
Adarsh Jha <jhaadarsh924@gmail.com>
2025-08-01 21:38:13 +0530

    Update main.py

--- Day39/main.py
+++ Day39/main.py
@@ -6,37 +6,37 @@ from flight_data import find_cheapest_flight
 from notification_manager import NotificationManager
 
 data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
 flight_search = FlightSearch()
 notification_manager = NotificationManager()
 
-sheet_data = data_manager.get_destination_data()
 ORIGIN_CITY_IATA = "LON"
 
 for row in sheet_data:
     if row["iataCode"] == "":
         row["iataCode"] = flight_search.get_destination_code(row["city"])
         time.sleep(2)
+print(f"sheet_data:\n {sheet_data}")
 
 data_manager.destination_data = sheet_data
 data_manager.update_destination_codes()
 
 tomorrow = datetime.now() + timedelta(days=1)
-six_months_later = datetime.now() + timedelta(days=6 * 30)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
 
 for destination in sheet_data:
-    print(f"Getting flights for {destination['city']}...")
-    flight_data = flight_search.check_flights(
+    print(f"Getting flights for {destination}")
+    flights = flight_search.check_flights(
         ORIGIN_CITY_IATA,
         destination["iataCode"],
         from_time=tomorrow,
-        to_time=six_months_later
+        to_time=six_month_from_today
     )
-    cheapest = find_cheapest_flight(flight_data)
-
-    if cheapest.price != "N/A" and cheapest.price < destination["lowestPrice"]:
-        message = (
-            f"✈️ Low price alert! Only £{cheapest.price} to fly from "
-            f"{cheapest.origin_airport} to {cheapest.destination_airport}, "
-            f"{cheapest.out_date} to {cheapest.return_date}."
+    cheapest_flight = find_cheapest_flight(flights)
+    if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
+        print(f"Lower price flight found to {destination['city']}!")
+        notification_manager.send_sms(
+            message_body=f"Low price alert! Only £{cheapest_flight.price} to fly "
+                         f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
+                         f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
         )
-        notification_manager.send_telegram(message)
