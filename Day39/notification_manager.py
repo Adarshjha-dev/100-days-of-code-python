8b5130105b4137329e12ad0f3e8da0bb6eb65083
Adarsh Jha <jhaadarsh924@gmail.com>
2025-08-01 21:38:27 +0530

    Update notification_manager.py

--- Day39/notification_manager.py
+++ Day39/notification_manager.py
@@ -4,24 +4,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TELEGRAM_BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
-TELEGRAM_CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
-TELEGRAM_API_URL = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-
-
 class NotificationManager:
-    def send_telegram(self, message):
-        payload = {
-            "chat_id": TELEGRAM_CHAT_ID,
-            "text": message,
-            "parse_mode": "HTML"
-        }
+    def __init__(self):
+        self.token = os.environ["TELEGRAM_BOT_TOKEN"]
+        self.chat_id = os.environ["TELEGRAM_CHAT_ID"]
+        self.api_url = f"https://api.telegram.org/bot{self.token}/sendMessage"
 
-        response = requests.post(TELEGRAM_API_URL, data=payload)
-        try:
-            response.raise_for_status()
-            print("✅ Telegram message sent successfully.")
-        except requests.exceptions.RequestException as e:
-            print("❌ Failed to send Telegram message.")
-            print(e)
-            print(response.text)
+    def send_sms(self, message_body):
+        response = requests.post(
+            url=self.api_url,
+            data={
+                "chat_id": self.chat_id,
+                "text": message_body
+            }
+        )
+        if response.status_code != 200:
+            print("Failed to send Telegram message:", response.text)
+        else:
+            print("Telegram message sent successfully")
